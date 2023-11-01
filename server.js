8a5c34cc0040c35f5233a6c92fc4b3167f848cd4
colby <colbyx20@gmail.com>
2023-11-01 11:37:33 -0400

    modifying server.js file

--- server.js
+++ server.js
@@ -2,15 +2,23 @@ const bodyParser = require('body-parser')
 const express = require('express')
 
 const app = express()
-app.use(bodyParser.json)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/static', express.static('public'));
 
 app.get("/", (res,req) => {
-    write.json("something");
+    res.send("hello world")
+})
+
+app.get("/user/:userId", (req,res) =>{
+    res.send(req.params);
 })
 
 app.post("/user", (res,req) =>{
-    const user = res.param("name");
-    write.json("user")
+    const user = req.body.name; // form name
+    // do some db stuff
+
+    res.send(`Creating a Post Request ${user}`)
 })
 
 app.listen("listening on port", 4000);
